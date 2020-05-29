5ac47489b9ead38fce81385e93b2ce282e302c7d
Sujin Lee <sjnu21@naver.com>
2020-05-29 11:16:37 +0900

    29 May 2020 Class

--- sistg-react-project2/recipe-server.js
+++ sistg-react-project2/recipe-server.js
@@ -11,12 +11,54 @@ app.listen(port,()=>{
     console.log("Start Server....","http://localhost:3355")
 })
 
+// [CORS 해결]: port가 달라도 허용해주도록
+// 참고) http://guswnsxodlf.github.io/enable-CORS-on-express
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.get('/',(req,res)=>{
     res.send('Hello Node.js')
 })
 
+const MongoConnect=require("mongodb").MongoClient; // MongoDB 연결 객체 생성
+/*  Java에서의 아래 코딩과 동일.
+    @RequestMapping("/recipe_data")
+    public String ...
+ */
+
 app.get('/recipe_data',(req,res)=>{
-    res.send('Hello This is recipe data')
+    /*res.send('Hello This is recipe data')*/ /* send('문자열') */
+
+    // 페이지 받아오기
+    var page=req.query.page; // Java에서의 String page=request.getParameter("page");
+    // url이 '/recipe_data?page=1' 요런식으로 들어옴 ==> 'req.query.변수명'에서 변수명은 url 파라미터명과 일치해야함.
+    var rowSize=12; // 한 페이지에 20개 출력
+    var skip=(page)*(rowSize-1);
+    var url="mongodb://211.238.142.181:27017"
+    // MongoDB와 연결 => 연결 객체 얻기
+    // MongoClient MongoConnect=new MongoClient()
+    /*                  Oracle          MongoDB
+        database:         XE             mydb
+                         table          collection
+     */
+    MongoConnect.connect(url,function (err,client) {
+        var db=client.db('mydb'); // XE
+        db.collection('recipe').find({}).skip(skip).limit(rowSize).toArray(function (err,docs) {
+            res.json(docs);  // Java에서의 model.addAttribute("list",list);와 동일. JavaScript는 Java와 달리 ArrayList가 없으므로 배열로 보냄.
+            // 근데, 배열 docs [{},{}]는 문자로 인식되므로 json(docs)해서 JSON으로 변환했다.
+            client.close();
+        })
+        // find(): SELECT
+        /* toArray
+           - MongoDB는 배열이 아님 ==> toArray는 {} {} {} {} ... {} 이런 애들을 [{} {} {} {}] 이렇게 배열로 만들어 준다.
+           - (Oracle에서 배열이 아니라서 ArrayList로 만드는 것과 유사.
+           - docs=[{} {} {} {} ] 요렇게 된다. */
+
+
+    })
 })
 
 
--- sistg-react-project2/src/components/Recipe.js
+++ sistg-react-project2/src/components/Recipe.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import axios from 'axios';
+// useEffect: 데이터를 가지고 옴. (서버나 파일을 읽어온다.)
+// useState: 저장. (읽어온 파일을 저장)
 
 export default function Recipe(props) {
+    const [recipe,setRecipe]=useState([]) // recipe-server.js에서 배열로 보냈으니까 배열로 받아야.
+    const [page,setPage]=useState(1)
+    const[total,setTotal]=useState(0)
+    /*  위의 코딩은, 이전에 함수가 아니라 class로 React 짰을 때의 아래의 코딩과 같다.
+        this.set{
+            page:1,
+            total:0
+        }
+     */
+
+    useEffect(()=>{
+        axios.get('http://localhost:3355/recipe_data',{
+            params:{
+                page:page // http://localhost:3355/recipe_data?page='페이지변수값' 이렇게 됨
+            }
+        }).then((result)=>{
+            setRecipe(result.data);
+        })
+        // 여기서 axios.get했으니까 recipe-server.js에서 app.get 해야함
+        // 물론, axios.post도 있다.
+    })
+    /*  위의 useEffect 코딩은, 이전에 함수가 아니라 class로 React 짰을 때의 componentWillMount()와 같다. 데이터를 가지고 온다. */
+    /*
+        [참고] 함수 안에 함수를 만드는 것은 불가능
+        ==> 함수로 React를 만들면(export default function Recipe(props){...}),
+            class로 React 프로그램 만들 때와는 달리,
+            React lifecyle 관련 함수들(componentWillMount(), componentDidMount()와 같은 함수를 쓸 수 없다.
+            왜냐면 함수 안에 함수를 또 쓸 수 없으니까...
+            ==> Hooks(useEffect 등)를 사용.
+                - ★ useEffect를 이용하면 함수 안에 함수를 만들 수 있다. ★
+        [참고] Java에서, class 안에 class는 가능. (inner class) - 우리 쓰레드 만들 때 썼었음.
+        class A{ class B{} }
+     */
+
+    const html=recipe.map((m)=>
+        <div className="col-md-3">
+            <div className="thumbnail">
+                <img src={m.poster} alt="Lights" style={{"width":"100%"}}/>
+                <div className="caption">
+                    <p style={{"font-size":"9pt"}}>{m.title}</p>
+                    <sub style={{"color":"gray"}}></sub>
+                </div>
+            </div>
+        </div>
+    )
+
     return(
-        <h1 className={"text-center"}>레시피 메인</h1>
+        <React.Fragment>
+            <div className={"row"}>
+                <h3 className={"text-center"}>레시피 메인</h3>
+                {html}
+            </div>
+            <div className={"row"}>
+                <button className={"btn btn-lg btn-primary"}>이전</button>
+                <button className={"btn btn-lg btn-danger"}>다음</button>
+            </div>
+        </React.Fragment>
     )
 }
